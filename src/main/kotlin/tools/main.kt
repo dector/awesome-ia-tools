7c2f17ec6b29532cc43180c898147044d855346f
Denys M <dector9@gmail.com>
2020-07-02 19:10:38 +0400

    Add bare-bone processing method

--- src/main/kotlin/tools/main.kt
+++ src/main/kotlin/tools/main.kt
@@ -1,5 +1,20 @@
 package tools
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 fun main() {
-    println("It works!")
+    val path = Paths.get("data")
+
+    verifyRepo(path)
+    processRepo(path)
+}
+
+private fun verifyRepo(path: Path) {
+    require(Files.exists(path))
+}
+
+private fun processRepo(path: Path) {
+    TODO()
 }
